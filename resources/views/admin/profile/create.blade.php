d86b6246be22f10dbd3b6ded5172f8c77552b59c
Yasuhiro Terada <teradadadadada@gmail.com>
2020-08-22 20:45:22 +0000

    add new file

---
+++ resources/views/admin/profile/create.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Mynews</title>
+    </head>
+    <body>
+        <header>
+            <div class="container">
+                <div class="header-logo"></div>
+                <div class="header-list"></div>
+            </div>
+        </header>
+        <main>
+            <div class="container">
+                <div class="top-message"></div>
+                <div class="contents"></div>
+                    <p>aaaaaaa</p>
+                    <p>bbbbbbb</p>
+                    <p>ccccccc</p>
+            </div>
+        </main>
+        <footer>
+            <div class ="container"></div>
+                <div class ="footer-logo"></div>
+                <p class="footer-message"></p>
+        </footer>
+    </body>
+</html>
