f4ae4d99c5d775b28531fce2189be55f79abee35
Yasuhiro Terada <teradadadadada@gmail.com>
2020-08-25 13:40:02 +0000

    submit lesson 12

--- routes/web.php
+++ routes/web.php
@@ -20,12 +20,16 @@ Route::get('/', function () {
 
 //課題4
 Route::group(['prefix' => 'admin'], function(){
-    Route::get('profile/create','Admin\ProfileController@add');
-    Route::get('profile/edit','Admin\ProfileController@edit');
+    Route::get('profile/create','Admin\ProfileController@add')>middleware('auth');
+    Route::get('profile/edit','Admin\ProfileController@edit')>middleware('auth');
 });
 
 
 Route::group(['prefix' => 'admin'], function(){
-    Route::get('news/create', 'Admin\NewsController@add');
+    Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');
 });
 
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
