f9e839a0f4b527547af993195bac2240532a9e46
Yasuhiro Terada <teradadadadada@gmail.com>
2020-08-27 21:04:27 +0000

    lesson 16

--- app/Http/Controllers/Admin/NewsController.php
+++ app/Http/Controllers/Admin/NewsController.php
@@ -88,5 +88,15 @@ class NewsController extends Controller
       return redirect('admin/news');
   }
   
+// 以下を追記　　
+  public function delete(Request $request)
+  {
+      // 該当するNews Modelを取得
+      $news = News::find($request->id);
+      // 削除する
+      $news->delete();
+      return redirect('admin/news/');
+  } 
+  
   
 }
--- routes/web.php
+++ routes/web.php
@@ -44,6 +44,7 @@ Route::group(['prefix' => 'admin'], function() {
     Route::get('news', 'Admin\NewsController@index')->middleware('auth'); // 追記
     Route::get('news/edit', 'Admin\NewsController@edit')->middleware('auth'); // 追記
     Route::post('news/edit', 'Admin\NewsController@update')->middleware('auth'); // 追記
+    Route::get('news/delete', 'Admin\NewsController@delete')->middleware('auth');
 });
 
 
