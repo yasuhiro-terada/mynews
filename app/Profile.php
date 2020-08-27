e6977f0ca6712b41420faa30517ac46abf9c79d7
Yasuhiro Terada <teradadadadada@gmail.com>
2020-08-27 13:28:47 +0000

    add new file

--- app/Profile.php
+++ app/Profile.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Profile extends Model
 {
+    protected $guarded =array('id');
     //
     public static $rules =array(
         'name'=> 'required',
