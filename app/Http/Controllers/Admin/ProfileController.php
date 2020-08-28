258f0aa0fe0c390875c106c05d77322730e61429
Yasuhiro Terada <teradadadadada@gmail.com>
2020-08-28 12:59:10 +0000

    lesson16

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -22,13 +22,7 @@ class ProfileController extends Controller
         $profile = new Profile;
         $form = $request->all();
 
-      // フォームから画像が送信されてきたら、保存して、$news->image_path に画像のパスを保存する
-      if (isset($form['image'])) {
-        $path = $request->file('image')->store('public/image');
-        $profile->image_path = basename($path);
-      } else {
-          $profile->image_path = null;
-      }
+     
 
       // フォームから送信されてきた_tokenを削除する
       unset($form['_token']);
